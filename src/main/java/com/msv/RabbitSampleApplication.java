b2aab871e41592eff1d199f11db1a433e56ce704
vinicius.marques <msvinicius27@gmail.com>
2025-07-15 18:33:09 -0300

    feat: first commit spring initializer

---
+++ src/main/java/com/msv/RabbitSampleApplication.java
@@ -0,0 +1,13 @@
+package com.msv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msv/RabbitSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
